ccb0eb130fd2a0d83f3a4dcb175c06c47edacd5e
JavierReyesLosEnlaces <119584431+JavierReyesLosEnlaces@users.noreply.github.com>
2023-09-29 00:22:52 +0200

    Dictagame listo para enviar

--- Dictagame/js/borrar_tabla.js
+++ Dictagame/js/borrar_tabla.js
@@ -1,10 +1,6 @@
-// Función para borrar la tabla usuarios
+// Función para borrar la tabla "USUARIOS"
 function borrarTablaUsuarios() {
   var request = window.indexedDB.open('mi_base_de_datos', 1);
-
-  request.onerror = function (event) {
-    console.log("Error al abrir la base de datos: " + event.target.errorCode);
-  };
 
   request.onsuccess = function (event) {
     var db = event.target.result;
@@ -18,24 +14,19 @@ function borrarTablaUsuarios() {
     // Borrar la tabla de usuarios
     var deleteRequest = objectStore.clear();
 
+
     deleteRequest.onsuccess = function () {
       console.log("Tabla 'usuarios' borrada con éxito.");
     };
 
-    deleteRequest.onerror = function (event) {
-      console.log("Error al borrar la tabla 'usuarios': " + event.target.error);
-    };
 
-    // Completar la transacción
+    // se completa la transacción y se cierra
     transaction.oncomplete = function () {
       db.close();
     };
   };
+};
 
-  request.onblocked = function (event) {
-    console.log("La base de datos está bloqueada debido a otra conexión abierta.");
-  };
-}
 
-// Ejemplo de uso
-borrarTablaUsuarios();
+
+
