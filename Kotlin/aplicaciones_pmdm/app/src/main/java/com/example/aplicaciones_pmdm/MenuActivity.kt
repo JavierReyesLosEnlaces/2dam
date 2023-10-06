c9f00a0fc5d0c58cb64e05353e383c11e08bc902
JavierReyesLosEnlaces <javi.reyes.losenlaces@gmail.com>
2023-10-06 19:17:11 +0200

    IMC

---
+++ Kotlin/aplicaciones_pmdm/app/src/main/java/com/example/aplicaciones_pmdm/MenuActivity.kt
@@ -0,0 +1,43 @@
+package com.example.aplicaciones_pmdm
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.aplicaciones_pmdm.HelloApp.MainActivity
+import com.example.aplicaciones_pmdm.IMCApp.IMCCalculatorActivity
+import com.example.aplicaciones_pmdm.MessageApp.MessageActivity
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+        //HELLOAPP
+        var btnHelloApp = findViewById<Button>(R.id.btnHelloApp);
+        btnHelloApp.setOnClickListener{navigateToHelloApp()}
+
+        //MESSAGE_ACTIVITY
+        var btnMessageApp = findViewById<Button>(R.id.btnMessageActivity);
+        btnMessageApp.setOnClickListener{navigateToMessageActivity()}
+
+        //IMC ACTIVITY
+        var btnIMCApp = findViewById<Button>(R.id.btnIMCActivity);
+        btnIMCApp.setOnClickListener{navigateToIMCActivity()}
+    }
+
+    private fun navigateToHelloApp() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToMessageActivity() {
+        val intent = Intent(this, MessageActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToIMCActivity() {
+        val intent = Intent(this, IMCCalculatorActivity::class.java)
+        startActivity(intent)
+    }
+}
