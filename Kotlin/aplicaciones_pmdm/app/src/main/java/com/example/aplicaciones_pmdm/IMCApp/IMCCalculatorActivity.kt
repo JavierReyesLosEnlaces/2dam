378e4e7b98efe595ad3c1637ca3d1b5f5e0a31ef
JavierReyesLosEnlaces <119584431+JavierReyesLosEnlaces@users.noreply.github.com>
2023-10-08 18:28:23 +0200

    Actualiz: IMC

--- Kotlin/aplicaciones_pmdm/app/src/main/java/com/example/aplicaciones_pmdm/IMCApp/IMCCalculatorActivity.kt
+++ Kotlin/aplicaciones_pmdm/app/src/main/java/com/example/aplicaciones_pmdm/IMCApp/IMCCalculatorActivity.kt
@@ -1,12 +1,25 @@
 package com.example.aplicaciones_pmdm.IMCApp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.aplicaciones_pmdm.MenuActivity
 import com.example.aplicaciones_pmdm.R
 
 class IMCCalculatorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imccalculator)
+
+        //BOTÓN VOLVER
+        var btnVol= findViewById<Button>(R.id.btnVolverAlMenu)
+        btnVol.setOnClickListener{volverMenu()}
+    }
+
+
+    private fun volverMenu(){
+        val intent = Intent(this, MenuActivity::class.java)
+        startActivity(intent)
     }
 }
