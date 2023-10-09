ef8c79d17c1c19a666d7de10405595c60eb036a4
JavierReyesLosEnlaces <javi.reyes.losenlaces@gmail.com>
2023-10-09 17:37:17 +0200

    Hamburguesas

---
+++ C#/Hamburguesería/Hamburguesa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class Hamburguesa
+{
+    private bool panCenteno;
+    private bool carneVacuno;
+    private float precioBase;
+    private float precioFinal;
+
+    public Hamburguesa()
+    {
+        panCenteno = false;
+        carneVacuno = false;
+        precioBase = 5.0f;
+    }
+
+    // Properties (getters and setters)
+    public bool PanCenteno
+    {
+        get { return panCenteno; }
+        set { panCenteno = value; }
+    }
+
+    public bool CarneVacuno
+    {
+        get { return carneVacuno; }
+        set { carneVacuno = value; }
+    }
+
+    public float PrecioBase
+    {
+        get { return precioBase; }
+        set { precioBase = value; }
+    }
+}
