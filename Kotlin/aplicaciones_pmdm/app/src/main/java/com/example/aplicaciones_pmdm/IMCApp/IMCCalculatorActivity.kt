09f066fcbdcdc6a05b55c0ad52c01ca286b07f2b
JavierReyesLosEnlaces <javi.reyes.losenlaces@gmail.com>
2023-10-10 19:22:01 +0200

    Logic IMCActivity

--- Kotlin/aplicaciones_pmdm/app/src/main/java/com/example/aplicaciones_pmdm/IMCApp/IMCCalculatorActivity.kt
+++ Kotlin/aplicaciones_pmdm/app/src/main/java/com/example/aplicaciones_pmdm/IMCApp/IMCCalculatorActivity.kt
@@ -4,22 +4,60 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.cardview.widget.CardView
 import com.example.aplicaciones_pmdm.MenuActivity
 import com.example.aplicaciones_pmdm.R
 
 class IMCCalculatorActivity : AppCompatActivity() {
+
+    private var isMaleSelected: Boolean = false;
+    private var isFemaleSelected: Boolean = false;
+
+
+    //leclaramos estas variables fuera del onCreate para que sean globales
+    //globales -> accesibles desde cualquier parte de IMCCalculatorActivity
+
+    private lateinit var  viewMale: CardView
+    private lateinit var  viewFemale: CardView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imccalculator)
-
-        //BOTÓN VOLVER
-        var btnVol= findViewById<Button>(R.id.btnVolverAlMenu)
-        btnVol.setOnClickListener{volverMenu()}
+        initComponents()
+        initListeners()
     }
 
+    private fun initListeners() {
+        //para poner varias líneas de código metemos llaves, no paréntesis
 
-    private fun volverMenu(){
-        val intent = Intent(this, MenuActivity::class.java)
-        startActivity(intent)
+        viewMale.setOnClickListener{
+            setComponentColorMale();
+        }
+
+        viewFemale.setOnClickListener{
+            setComponentColorFemale();
+        }
+    }
+
+    private fun setComponentColorMale() {
+        //esta función comprueba si el cardview del nombre está seleccionado
+        if(!isMaleSelected){
+            viewMale.setBackgroundColor(getColor(R.color.background_component_selected))
+            viewFemale.setBackgroundColor(getColor(R.color.background_component))
+            isMaleSelected = true
+            isFemaleSelected = false
+        }
+    }
+
+    private fun setComponentColorFemale() {
+        TODO("Not yet implemented")
+    }
+
+    //las funciones fuera del onCreate para que esta actividad sea global
+    //pero SOLO en esta actividad
+
+    private fun initComponents(){
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
     }
 }
