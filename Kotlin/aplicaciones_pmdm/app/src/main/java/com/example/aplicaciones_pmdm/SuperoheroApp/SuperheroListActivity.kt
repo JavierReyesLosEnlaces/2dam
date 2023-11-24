07e45d3316ded6f02bf7610da83e7663930a12e7
JavierReyesLosEnlaces <javi.reyes.losenlaces@gmail.com>
2023-11-24 17:21:06 +0100

    Searchview, APIs

---
+++ Kotlin/aplicaciones_pmdm/app/src/main/java/com/example/aplicaciones_pmdm/SuperoheroApp/SuperheroListActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aplicaciones_pmdm.SuperoheroApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aplicaciones_pmdm.R
+
+class SuperheroListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_superhero_list)
+    }
+}
