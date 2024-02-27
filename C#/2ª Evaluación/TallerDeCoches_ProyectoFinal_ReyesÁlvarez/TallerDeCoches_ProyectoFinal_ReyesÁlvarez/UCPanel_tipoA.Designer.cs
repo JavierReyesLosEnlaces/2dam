294bc289c4073cd9ad3c9f3079100746db2f09d2
JavierReyesLosEnlaces <javi.reyes.losenlaces@gmail.com>
2024-02-27 19:12:11 +0100

    Fin martes

--- C#/2ª Evaluación/TallerDeCoches_ProyectoFinal_ReyesÁlvarez/TallerDeCoches_ProyectoFinal_ReyesÁlvarez/UCPanel_tipoA.Designer.cs
+++ C#/2ª Evaluación/TallerDeCoches_ProyectoFinal_ReyesÁlvarez/TallerDeCoches_ProyectoFinal_ReyesÁlvarez/UCPanel_tipoA.Designer.cs
@@ -28,37 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            tableLayoutPanel1 = new TableLayoutPanel();
+            tlp1 = new TableLayoutPanel();
+            tlp2 = new TableLayoutPanel();
+            tlp1.SuspendLayout();
             SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // tlp1
             // 
-            tableLayoutPanel1.ColumnCount = 2;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.Dock = DockStyle.Fill;
-            tableLayoutPanel1.Location = new Point(0, 0);
-            tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 1;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.Size = new Size(682, 400);
-            tableLayoutPanel1.TabIndex = 0;
+            tlp1.ColumnCount = 1;
+            tlp1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tlp1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tlp1.Controls.Add(tlp2, 0, 1);
+            tlp1.Dock = DockStyle.Fill;
+            tlp1.Location = new Point(0, 0);
+            tlp1.Margin = new Padding(0);
+            tlp1.Name = "tlp1";
+            tlp1.RowCount = 2;
+            tlp1.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+            tlp1.RowStyles.Add(new RowStyle(SizeType.Percent, 90F));
+            tlp1.Size = new Size(682, 400);
+            tlp1.TabIndex = 0;
+            // 
+            // tlp2
+            // 
+            tlp2.ColumnCount = 2;
+            tlp2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tlp2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tlp2.Dock = DockStyle.Fill;
+            tlp2.Location = new Point(0, 40);
+            tlp2.Margin = new Padding(0);
+            tlp2.Name = "tlp2";
+            tlp2.RowCount = 1;
+            tlp2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tlp2.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tlp2.Size = new Size(682, 360);
+            tlp2.TabIndex = 0;
             // 
             // UCPanel_tipoA
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(117, 230, 164);
-            Controls.Add(tableLayoutPanel1);
+            Controls.Add(tlp1);
             Margin = new Padding(10);
             Name = "UCPanel_tipoA";
             Size = new Size(682, 400);
+            tlp1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tlp1;
+        private TableLayoutPanel tlp2;
     }
 }
